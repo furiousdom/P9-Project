e33b43de6fd5465d21767da248ab1ace972e9441
icedandreas <a-hald@stofanet.dk>
2020-12-15 17:58:18 +0100

    Slight update

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ print('To start featurization please,')
 
 prompt1 = 'q'
 while prompt1 != '':
-    prompt1 = input('Specify the featurizer (rdkit, convmol, circprint, weave, molgraph, mol2vec, smile2image, onehot, coulombmatrix, KNNmol2vec): ')
-    if prompt1 == 'rdkit' or prompt1 == 'convmol' or prompt1 == 'circprint' or prompt1 == 'weave' or prompt1 == 'molgraph' or prompt1 == 'mol2vec' or prompt1 == 'smile2image' or prompt1 == 'onehot' or prompt1 == 'coulombmatrix' or prompt1 == 'KNNmol2vec':
+    prompt1 = input('Specify the featurizer (rdkit, convmol, circprint, weave, molgraph, mol2vec, smile2image, onehot, coulombmatrix, KNNmol2vec, manmol2vec): ')
+    if prompt1 == 'rdkit' or prompt1 == 'convmol' or prompt1 == 'circprint' or prompt1 == 'weave' or prompt1 == 'molgraph' or prompt1 == 'mol2vec' or prompt1 == 'smile2image' or prompt1 == 'onehot' or prompt1 == 'coulombmatrix' or prompt1 == 'KNNmol2vec' or prompt1 == 'manmol2vec':
         break
     prompt1 = ''
     print('Wrong input.')
--- featurizers.py
+++ featurizers.py
@@ -3,6 +3,7 @@ from rdkit import Chem# Might need to import this one in a different manor.
 from printer import mols_to_pngs, display_images
 from xdparser import parseSmiles
 import numpy
+from scipy.spatial import distance
 
 def smilesList(smiles_dic_values):
     """
@@ -38,6 +39,8 @@ def featurize_molecules(featurization=None, dataset=None):
         coulombMatrixFeaturization(smiles)
     if featurization == 'KNNmol2vec':
         KNNmol2VecFeaturization(smiles)
+    if featurization == 'manmol2vec':
+        Manhattanmol2VecFeaturization(smiles)
     
     """
     switch(featurization){
@@ -119,7 +122,7 @@ def mol2VecFeaturization(smiles):#Needs packages that are incompatible I think.
         k=k+1
         i=k+1
 
-#This is an attempt at creating a better mol2vec based KKN algorithm
+#This is an attempt at creating a better mol2vec based KKN algorithm ##############################################################
 class molecule: #This class is used to store objects in an array. Just holds the average distance for now.
     def __init__(self, average_distance, name):
         self.average_distance = average_distance
@@ -132,6 +135,7 @@ def KNNmol2VecFeaturization(smiles):#Needs packages that are incompatible I thin
     print(f'Length of features: {len(features)}')
     i = 20 #Just taking the first 20 molecules as my initial set. Need to change this to be the initial molecule set instead. Create the set by cutting it from the complete list.
     k = 0
+    
     combinedtempdistance = 0
     moleculelist = []
     while i<len(features):
@@ -152,7 +156,36 @@ def KNNmol2VecFeaturization(smiles):#Needs packages that are incompatible I thin
     for mole in moleculelist:
         print(mole.average_distance)
 
-############################################################################################################
+###############################################################################################################################
+
+def Manhattanmol2VecFeaturization(smiles):#Needs packages that are incompatible I think.
+    featurizer = dc.feat.Mol2VecFingerprint()
+    features = featurizer(smiles)
+    print(f'Number of featurized items: {len(features)} \nValues:')
+    print(f'Length of features: {len(features)}')
+    i = 20 #Just taking the first 20 molecules as my initial set. Need to change this to be the initial molecule set instead. Create the set by cutting it from the complete list.
+    k = 0
+    combinedtempdistance = 0
+    moleculelist = []
+    while i<len(features):
+        while k<20:       
+            if (len(features[k])!=0 and len(features[i])!=0):
+                combinedtempdistance = combinedtempdistance + distance.cdist(features[k], features[i], 'cityblock')#It compains that its not a 2D array. Maybe need to delimiter it with comma. Other option: numpy.abs(features[k] - features[i]).sum
+                k=k+1
+            else:
+                print('A molecule could not be featurized. Skipping ahead to next molecule.')
+                k=k+1
+        combinedtempdistance = combinedtempdistance / 20 #20 should be replaced by len(list of initial molecules)
+        Molecule = molecule(combinedtempdistance, i) #Might want to use the actual name instead of just its number in the index.
+        moleculelist.append(Molecule) #Index of array is same as the molecule number.
+        combinedtempdistance = 0
+        i=i+1
+        k=0
+    moleculelist.sort(key=lambda x: x.average_distance)
+    for mole in moleculelist:
+        print(mole.average_distance)
+
+###################################################################################################################################
 
 def smilesToImageFeaturization(smiles):
     # featurizer = dc.feat.SmilesToImage()
