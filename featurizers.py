8273267b05e0d5b11caeb1d7201ca63ef687a905
icedandreas <a-hald@stofanet.dk>
2020-12-17 12:09:37 +0100

    Added similarity ratio

--- featurizers.py
+++ featurizers.py
@@ -41,6 +41,8 @@ def featurize_molecules(featurization=None, dataset=None):
         KNNmol2VecFeaturization(smiles)
     if featurization == 'manmol2vec':
         Manhattanmol2VecFeaturization(smiles)
+    if featurization == 'simmol2vec':
+        Simmol2VecFeaturization(smiles)
     
     """
     switch(featurization){
@@ -122,7 +124,7 @@ def mol2VecFeaturization(smiles):#Needs packages that are incompatible I think.
         k=k+1
         i=k+1
 
-#This is an attempt at creating a better mol2vec based KKN algorithm ##############################################################
+####################################################   KNN   ###############################################################
 class molecule: #This class is used to store objects in an array. Just holds the average distance for now.
     def __init__(self, average_distance, name):
         self.average_distance = average_distance
@@ -157,7 +159,7 @@ def KNNmol2VecFeaturization(smiles):#Needs packages that are incompatible I thin
         print(mole.average_distance)
     
 
-###############################################################################################################################
+##########################################################   Manhattan   #####################################################################
 
 def Manhattanmol2VecFeaturization(smiles):#Needs packages that are incompatible I think.
     featurizer = dc.feat.Mol2VecFingerprint()
@@ -184,6 +186,37 @@ def Manhattanmol2VecFeaturization(smiles):#Needs packages that are incompatible 
         i=i+1
         k=0
     moleculelist.sort(key=lambda x: x.average_distance)
+    for mole in moleculelist:
+        print(mole.average_distance)
+
+############################################################   Similarity ratio   #######################################################################
+#Takes about 5-10 min to run with a target set of just 20. Maybe it can be optimized somehow?
+
+def Simmol2VecFeaturization(smiles):#Needs packages that are incompatible I think.
+    featurizer = dc.feat.Mol2VecFingerprint()
+    features = featurizer(smiles)
+    print(f'Number of featurized items: {len(features)} \nValues:')
+    print(f'Length of features: {len(features)}')
+    i = 20 #Just taking the first 20 molecules as my initial set. Need to change this to be the initial molecule set instead. Create the set by cutting it from the complete list.
+    k = 0
+    combinedtempdistance = 0
+    moleculelist = []
+    while i<len(features):
+        while k<20:       
+            if (len(features[k])!=0 and len(features[i])!=0):            
+                for x in range(0,300):
+                    combinedtempdistance = combinedtempdistance + ((features[k][x] * features[i][x]) / ((features[k][x] * features[i][x]) + (features[k][x] - features[i][x])**2))
+                k=k+1
+            else:
+                #print('A molecule could not be featurized. Skipping ahead to next molecule.')
+                k=k+1
+        combinedtempdistance = combinedtempdistance / 20 #20 should be replaced by len(list of initial molecules)
+        Molecule = molecule(combinedtempdistance, i) #Might want to use the actual name instead of just its number in the index.
+        moleculelist.append(Molecule) #Index of array is same as the molecule number.
+        combinedtempdistance = 0
+        i=i+1
+        k=0
+    moleculelist.sort(key=lambda x: x.average_distance, reverse=True)#Higher is better for this method. 
     for mole in moleculelist:
         print(mole.average_distance)
 
